6b6972db3b229030197ac6ddc0ca2eb06ba5d01c
gtsasiuk <gtsasuk@gmail.com>
2025-09-29 21:51:23 +0300

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import "./styles/main.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ContactsPage from "./pages/ContactsPage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
-            <HomePage />
-            <Footer />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/projects" element={<ProjectsPage />} />
+                    <Route path="/project" element={<ProjectPage />} />
+                    <Route path="/contacts" element={<ContactsPage />} />
+                </Routes>
+                <Footer />
+            </Router>
         </div>
     );
 }
