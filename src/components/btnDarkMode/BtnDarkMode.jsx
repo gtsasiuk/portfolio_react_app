165afcb9e14baf853bdae5fe6080aac5f2b462bc
gtsasiuk <gtsasuk@gmail.com>
2025-10-02 21:25:41 +0300

    Created BtnDarkMode as component

---
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -0,0 +1,18 @@
+import sun from "./sun.svg";
+import moon from "./moon.svg";
+import "./btnDarkMode.css";
+
+const BtnDarkMode = () => {
+    const toggleDarkMode = () => {
+        console.log("toggleDarkMode!"); 
+    }
+
+    return ( 
+        <button className="dark-mode-btn" onClick={toggleDarkMode}>
+            <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
+            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
+        </button>
+    );
+}
+ 
+export default BtnDarkMode;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,5 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../../img/icons/moon.svg";
-
 import { NavLink } from "react-router-dom";
-
+import BtnDarkMode from "../btnDarkMode/btnDarkMode";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -15,10 +12,7 @@ const Navbar = () => {
             <div className="container">
                 <div className="nav-row">
                     <NavLink to="/" className="logo"><strong>Freelancer</strong> portfolio</NavLink>
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
-                    </button>
+                    <BtnDarkMode />
                     <ul className="nav-list">
                         <li className="nav-list__item">
                             <NavLink to="./" className={({isActive}) =>  isActive ? activeLink : normalLink }>Home</NavLink>
