dfa7b6a0b31b080907618961ae980713c3f08c94
gtsasiuk <gtsasuk@gmail.com>
2025-10-06 17:42:59 +0300

    Added animation for toggling btn

--- src/components/btnDarkMode/BtnDarkMode.jsx
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -1,18 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 import "./btnDarkMode.css";
 
 const BtnDarkMode = () => {
     const[darkMode, setDarkMode] = useState('light');
+    const btnRef = useRef(null);
 
     useEffect(() => {
-        console.log("useEffect fired!"); 
-        console.log(darkMode); 
         if (darkMode === 'dark') {
             document.body.classList.add('dark');
+            btnRef.current.classList.add('dark-mode-btn--active');
         } else {
             document.body.classList.remove('dark');
+            btnRef.current.classList.remove('dark-mode-btn--active');
         }
     }, [darkMode]);
 
@@ -23,7 +24,7 @@ const BtnDarkMode = () => {
     }
 
     return ( 
-        <button className="dark-mode-btn" onClick={toggleDarkMode}>
+        <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
         </button>
